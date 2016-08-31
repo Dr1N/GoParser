56c89ffd0d6b49b5d5d83014195893bb29deee9f
Andrey <drn.exp@gmail.com>
2016-08-31 23:31:33 +0300

    EF Test

--- Parser/Parser/Main.cs
+++ Parser/Parser/Main.cs
@@ -1,17 +1,23 @@
-﻿using System;
+﻿using Parser.EF;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
+using xNet;
 
 namespace Parser
 {
     public partial class Main : Form
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public Main()
         {
             InitializeComponent();
@@ -19,7 +25,12 @@ namespace Parser
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
+            GoPrarserEntities context = new GoPrarserEntities();
+            DbSet<countries> countries = context.countries;
+            foreach (var county in countries)
+            {
+                logger.Trace(county.name);
+            }
         }
     }
 }
