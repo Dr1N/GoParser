fd6a9d756aa19fbb874367c1a1535a618b1f82ee
Andrey <drn.exp@gmail.com>
2016-10-17 00:23:53 +0300

    BrowsingContext

--- Parser/Parser/GoParser.cs
+++ Parser/Parser/GoParser.cs
@@ -1,10 +1,12 @@
 ﻿using AngleSharp.Parser.Html;
+using AngleSharp;
 using Parser;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Parser
 {
@@ -18,7 +20,7 @@ namespace Parser
 
         public GoParser()
         {
-
+            
         }
 
         public void ParseCities()
@@ -34,6 +36,22 @@ namespace Parser
         private List<countries> GetCountries()
         {
            return context.countries.ToList();
+        }
+
+        private async void GetPage()
+        {
+            // Setup the configuration to support document loading
+            var config = Configuration.Default.WithDefaultLoader();
+            // Load the names of all The Big Bang Theory episodes from Wikipedia
+            var address = "https://en.wikipedia.org/wiki/List_of_The_Big_Bang_Theory_episodes";
+            // Asynchronously get the document in a new context using the configuration
+            var document = await BrowsingContext.New(config).OpenAsync(address);
+            // This CSS selector gets the desired content
+            var cellSelector = "tr.vevent td:nth-child(3)";
+            // Perform the query to get all cells with the content
+            var cells = document.QuerySelectorAll(cellSelector);
+            // We are only interested in the text - select it with LINQ
+            var titles = cells.Select(m => m.TextContent);
         }
     }
 }
